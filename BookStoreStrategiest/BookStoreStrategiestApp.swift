a8cecace187f04a7a9567d82c47b458d6148cf21
Anton <kinzzersky@gmail.com>
2024-07-18 19:58:52 +0300

    Initial Commit

---
+++ BookStoreStrategiest/BookStoreStrategiestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreStrategiestApp.swift
+//  BookStoreStrategiest
+//
+//  Created by Anton on 18.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookStoreStrategiestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
